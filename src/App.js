30cd52fed896b385b13cb3dcb0a4098470e00199
MAKSYM_enko <finside41@gmail.com>
2019-03-13 22:47:48 +0200

    People and Films pages were added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { NavLink, Route, Switch} from 'react-router-dom'
 
 import HomePage from './HomePage';
+import FilmsPage from './FilmsPage';
+import PeoplePage from './PeoplePage';
 import './App.css';
 
 class App extends Component {
@@ -16,6 +18,8 @@ class App extends Component {
         <section>
           <Switch>
             <Route exact path="/" component={HomePage}/>
+            <Route exact path="/people" component={PeoplePage}/>
+            <Route exact path="/films" component={FilmsPage}/>
             <Route render={() => <h1>Page not found</h1>}/>
           </Switch>
           </section>  
