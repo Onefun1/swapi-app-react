7e17d3e2c99ce771d596c2176a9f965fe2d4d093
MAKSYM_enko <finside41@gmail.com>
2019-03-13 18:26:20 +0200

    some changes

---
+++ build/precache-manifest.ae7f19bfda99f868a04cd446d387b8b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/swapi-app-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7c6aaab5f16a6b73a85c",
+    "url": "/swapi-app-react/static/js/runtime~main.5b4569a0.js"
+  },
+  {
+    "revision": "ab9a6f2c4771d5bb0bfb",
+    "url": "/swapi-app-react/static/js/main.210f4182.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/swapi-app-react/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "ab9a6f2c4771d5bb0bfb",
+    "url": "/swapi-app-react/static/css/main.13e480e6.chunk.css"
+  },
+  {
+    "revision": "c70ffc0a80f6a57ef66f36cee01f0cd5",
+    "url": "/swapi-app-react/index.html"
+  }
+];
